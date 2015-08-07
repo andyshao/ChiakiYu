90f7b197a7a6bb212e14d56572e1536428a9525e
醉丶千秋 <yu_199012@qq.com>
2015-08-07 18:09:27 +0800

    sugnalR初接触

---
+++ ChiakiYu.Web/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ChiakiYu.Web.Startup))]
+namespace ChiakiYu.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
