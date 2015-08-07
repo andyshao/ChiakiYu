a6a2195704f736a0c8ed09d69772ffdb8a3a3b97
醉丶千秋 <yu_199012@qq.com>
2015-08-07 18:09:27 +0800

    signalR初接触

---
+++ ChiakiYu.Web/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace ChiakiYu.Web
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void Send(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
